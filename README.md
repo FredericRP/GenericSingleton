ea6492e816369e23b9bf0da971c4873bf6266717
FredericRP <frederic@rolland-porche.fr>
2021-09-15 22:55:04 +0200

    GenericSingleton: first release on its own repository

---
+++ README.md
@@ -0,0 +1,24 @@
+# Singleton
+
+A singleton is a development pattern that prevents having multiple instances of the same component in a scene.
+It allows also any script to access this one wihout requiring a manual link between the two.
+
+This one is a generic Singleton that allows you to inherit from it from your class to access its instance with the static Instance property.
+
+## Usage
+
+Extends the Singleton<T> class from your own classes like this :
+
+```C#
+public class MyBrandNewClass : Singleton<MyBrandNewClass> {
+// ...
+}
+```
+
+You can call it from other scripts like this :
+
+```C#
+MyBrandNewClass.Instance.PublicMethod()
+```
+
+See Transition class for a demo.
---
+++ Runtime/Scripts/Singleton.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System;
+
+namespace FredericRP.GenericSingleton
+{
+  /// <summary>
+  /// Singleton attribute work with singleton class.
+  /// </summary>
+  public class SingletonAttribute : Attribute
+  {
+    // Tells if this singleton has to create himself if he is called and not present in the scene.
+    public bool createIfNotPresent;
+
+    public SingletonAttribute(bool b)
+    {
+      createIfNotPresent = b;
+    }
+  }
+
+  /// <summary>
+  /// Be aware this will not prevent a non singleton constructor
+  ///   such as `T myT = new T();`
+  /// To prevent that, add `protected T () {}` to your singleton class.
+  /// 
+  /// As a side note, this is made a MonoBehaviour because we need Coroutines.
+  /// </summary>
+  //[ExecuteInEditMode]
+  [SingletonAttribute(true)]
+  public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+  {
+    private static T _instance = null;
+    private static bool applicationIsQuitting = false;
+
+    public static T Instance
+    {
+      get
+      {
+        if (_instance == null)
+        {
+          _instance = (T)FindObjectOfType(typeof(T));
+
+          if (_instance == null)
+          {
+            if (!applicationIsQuitting)
+            {
+              var customAttributes = (SingletonAttribute[])typeof(T).GetCustomAttributes(typeof(SingletonAttribute), true);
+
+              if (customAttributes.Length > 0)
+              {
+#if UNITY_EDITOR
+                Debug.Log("[Singleton] An instance of " + typeof(T) + " is needed in the scene");
+#endif
+
+                var myAttribute = customAttributes[0];
+                bool createNewSingleton = myAttribute.createIfNotPresent;
+
+                if (createNewSingleton)
+                {
+                  GameObject singleton = new GameObject();
+                  singleton.name = typeof(T).Name;
+                  _instance = singleton.AddComponent<T>();
+#if UNITY_EDITOR
+                  Debug.Log("[Singleton] An instance of " + typeof(T) + " was created");
+#endif
+                  _instance.OnAwake();
+                }
+              }
+              else
+              {
+#if UNITY_EDITOR
+                Debug.Log("[Singleton] An instance of " + typeof(T) + " is needed in the scene");
+#endif
+                GameObject singleton = new GameObject();
+                singleton.name = typeof(T).Name;
+                _instance = singleton.AddComponent<T>();
+#if UNITY_EDITOR
+                Debug.Log("[Singleton] An instance of " + typeof(T) + " was created");
+#endif
+                _instance.OnAwake();
+              }
+            }
+          }
+          else
+            _instance.OnAwake();
+        }
+        return _instance;
+      }
+    }
+
+    public virtual void OnAwake() { }
+
+    void Awake()
+    {
+      if (!_instance)
+      {
+        _instance = this as T;
+        _instance.OnAwake();
+      }
+      CheckMultipleInstance();
+    }
+
+    static void CheckMultipleInstance()
+    {
+      if (FindObjectsOfType(typeof(T)).Length > 1)
+      {
+#if UNITY_EDITOR
+        Debug.LogError("[Singleton<" + typeof(T) + ">] Something went really wrong " + " - there should never be more than 1 singleton!" + " Reopenning the scene might fix it.");
+#endif
+      }
+    }
+
+    public virtual void OnApplicationQuit()
+    {
+      applicationIsQuitting = true;
+    }
+  }
+}
